8cbb8e36e1f19c1cf0b9050b8f0c395610725410
Jesse Chan <jc@lineageos.org>
2020-09-11 20:44:44 +0300

    ARM64: configs: samsung: afaneh_a11q: enable DT building in defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,7 @@ export ARCH=arm64
 export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android samsung/a02q/sdm450_sec_a02q_eur_opn_defconfig
-make -j16 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android CONFIG_BUILD_ARM64_DT_OVERLAY=y
+make -j16 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
 
